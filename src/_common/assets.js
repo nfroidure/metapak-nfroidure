47a91688b6b144133dd7e3c6232c728bed340473
Nicolas Froidure (7Digital) <nicolas.froidure@insertafter.com>
2017-02-04 10:07:00 +0100

    Adding editor config + renamin files

--- src/_common/assets.js
+++ src/_common/assets.js
@@ -1,10 +1,17 @@
 'use strict';
 
 module.exports = (file, packageConf) => {
+  // Rename dot files the right way
+  if(file.name.startsWith('_dot_')) {
+    file.name = file.name.replace('_dot_', '.');
+  }
+
+  // Add author to the license
   if('LICENSE' === file.name) {
     file.data = file.data.replace(/<copyright holders>/mg, 'Nicolas Froidure');
   }
 
+  // Set files to code climate
   if('.codeclimate.yml' === file.name) {
     if(
       packageConf.metapak &&
@@ -14,5 +21,6 @@ module.exports = (file, packageConf) => {
       file.data = file.data.replace(/\*\*\.js/mg, packageConf.metapak.data.files);
     }
   }
+
   return file;
 };
