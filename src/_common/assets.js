6b786e894cd8157fb9523ca723d02b115c50149b
Nicolas Froidure <nicolas.froidure@insertafter.com>
2018-03-18 12:07:02 +0100

    feat(Common config): Allow to add .gitignore files to the base asset

--- src/_common/assets.js
+++ src/_common/assets.js
@@ -2,7 +2,12 @@
 
 const config = require('../config.js');
 
-module.exports = file => {
+module.exports = (file, packageConf) => {
+  const metapakData =
+    packageConf.metapak && packageConf.metapak.data
+      ? packageConf.metapak.data
+      : {};
+
   // Rename dot files the right way
   if (file.name.startsWith('_dot_')) {
     file.name = file.name.replace('_dot_', '.');
@@ -16,6 +21,19 @@ module.exports = file => {
   // Add NodeJS LTS where needed
   if (['.github/ISSUE_TEMPLATE'].includes(file.name)) {
     file.data = file.data.replace(/<lastNodeLTS>/gm, config.lastNodeLTS);
+  }
+
+  // Add .gitignore additionnal files
+  if (
+    file.name.endsWith('.gitignore') &&
+    metapakData.ignore &&
+    metapakData.ignore.length
+  ) {
+    file.data =
+      file.data +
+      '# Project custom ignored file\n' +
+      metapakData.ignore.join('\n') +
+      '\n';
   }
 
   return file;
