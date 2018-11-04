d7958842c66f46f316b6e718354e3fbac4e07146
Nicolas Froidure <nicolas.froidure@gmail.com>
2018-11-04 12:33:41 +0100

    refactor(Node support): Drop support for NodeJS 6
    NodeJS 8 adds support for async/await solving a lot of pain and Node 6 LTS ends in april 2019 so
    removing its support from my modules.
    
    BREAKING CHANGE: Will necessit to upgrade NodeJS to 8.12+

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // DRY, set here vars that may be used several times
 // in your metapak module
 module.exports = {
-  lastNodeLTS: '6.9.5',
+  lastNodeLTS: '8.12.0',
   lastNode: '10.0.0',
   apiPath: 'API.md',
 };
